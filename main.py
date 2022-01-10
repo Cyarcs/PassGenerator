8b0a8709814eb443111156403f3f92771054c158
Cyarcs <karavanoval@gmail.com>
2022-01-10 20:45:43 +0300

    Fixed code

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ from PyQt6 import QtWidgets, QtGui
 from Resource.PassGen import Ui_MainWindow
 
 
-class myWindow(QtWidgets.QMainWindow):
+class MyWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
-        super(myWindow, self).__init__()
+        super(MyWindow, self).__init__()
         self.rand_lower = None
         self.rand_upper = None
+        self.typePasWord = None
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.btn_copy.clicked.connect(self.copyPass)
-        self.ui.chb_number.stateChanged.connect(self.configPass)
-        self.ui.chb_char.stateChanged.connect(self.configPass)
-        self.ui.le_len_pass.textChanged.connect(self.configPass)
-        self.ui.rb_one.clicked.connect(self.configPass)
-        self.ui.rb_two.clicked.connect(self.configPass)
-        self.ui.rb_three.clicked.connect(self.configPass)
+        self.ui.btn_copy.clicked.connect(self.copypass)
+        self.ui.chb_number.stateChanged.connect(self.configpass)
+        self.ui.chb_char.stateChanged.connect(self.configpass)
+        self.ui.le_len_pass.textChanged.connect(self.configpass)
+        self.ui.rb_one.clicked.connect(self.configpass)
+        self.ui.rb_two.clicked.connect(self.configpass)
+        self.ui.rb_three.clicked.connect(self.configpass)
         self.UPPERCASE_CHARACTERS = string.ascii_uppercase
         self.LOWERCASE_CHARACTERS = string.ascii_lowercase
         self.DIGITS = string.digits
@@ -29,9 +30,9 @@ class myWindow(QtWidgets.QMainWindow):
         self.rand_digit = ''
         self.rand_symbol = ''
         self.lenPass = None
-        self.configPass()
+        self.configpass()
 
-    def configPass(self):
+    def configpass(self):
         if self.ui.le_len_pass.text() == '':
             self.ui.le_len_pass.setText('0')
             self.lenPass = 0
@@ -52,30 +53,31 @@ class myWindow(QtWidgets.QMainWindow):
             self.rand_symbol = random.choice(self.SYMBOLS)
         else:
             self.rand_symbol = ''
-        self._typePassWord()
-        self.genPass(self.typePassWord, self.lenPass)
+        self.typepassword()
+        self.genpass(self.typePasWord, self.lenPass)
 
-    def _typePassWord(self):
+    def typepassword(self):
         if self.ui.rb_one.isChecked():
-            self.typePassWord = 1
+            self.typePasWord = 1
         elif self.ui.rb_two.isChecked():
-            self.typePassWord = 2
+            self.typePasWord = 2
         else:
-            self.typePassWord = 3
+            self.typePasWord = 3
 
-    def _randomChoice(self, types):
+    @staticmethod
+    def randomchoice(types):
         return random.choice(types)
 
-    def genPass(self, typePass, lenpass):
+    def genpass(self, typepass, lenpass):
         word = ''
         for i in range(lenpass):
-            self.rand_upper = self._randomChoice(self.UPPERCASE_CHARACTERS)
-            self.rand_lower = self._randomChoice(self.LOWERCASE_CHARACTERS)
+            self.rand_upper = self.randomchoice(self.UPPERCASE_CHARACTERS)
+            self.rand_lower = self.randomchoice(self.LOWERCASE_CHARACTERS)
             if self.ui.chb_number.isChecked():
-                self.rand_digit = self._randomChoice(self.DIGITS)
+                self.rand_digit = self.randomchoice(self.DIGITS)
             if self.ui.chb_char.isChecked():
-                self.rand_symbol = self._randomChoice(self.SYMBOLS)
-            if typePass == 1:
+                self.rand_symbol = self.randomchoice(self.SYMBOLS)
+            if typepass == 1:
                 if len(word) != lenpass:
                     word = f"{word}{self.rand_upper}{self.rand_lower}{self.rand_digit}{self.rand_symbol}"
                     if len(word) < lenpass:
@@ -85,7 +87,7 @@ class myWindow(QtWidgets.QMainWindow):
                         break
                 else:
                     break
-            elif typePass == 2:
+            elif typepass == 2:
                 if len(word) != lenpass:
                     word = f"{word}{self.rand_upper}{self.rand_lower}{self.rand_digit}{self.rand_symbol}"
                     if len(word) < lenpass:
@@ -102,20 +104,21 @@ class myWindow(QtWidgets.QMainWindow):
                         continue
                     else:
                         self.ui.passWord.setText(
-                            f"{word[0:int(lenpass / 3)]}-{word[int(lenpass / 3):int(lenpass / 3) + int(lenpass / 3)]}-{word[int(lenpass / 3) + int(lenpass / 3):]}")
+                            f"{word[0:int(lenpass / 3)]}-{word[int(lenpass / 3):int(lenpass / 3) + int(lenpass / 3)]}"
+                            f"-{word[int(lenpass / 3) + int(lenpass / 3):]}")
                         break
                 else:
                     break
 
-    def copyPass(self):
-        clipPass = self.ui.passWord.text()
+    def copypass(self):
+        clippass = self.ui.passWord.text()
         ccc = QtGui.QGuiApplication.clipboard()
         ccc.clear()
-        ccc.setText(clipPass)
+        ccc.setText(clippass)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    application = myWindow()
+    application = MyWindow()
     application.show()
     sys.exit(app.exec())
